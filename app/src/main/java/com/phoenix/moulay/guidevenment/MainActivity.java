6442427a919d4e8f6dd6ca8c7688db0c9702f1b1
moulayprogramer <moulay0311@gmail.com>
2017-12-11 11:21:51 +0100

    splash an main struct

--- app/src/main/java/com/phoenix/moulay/guidevenment/MainActivity.java
+++ app/src/main/java/com/phoenix/moulay/guidevenment/MainActivity.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 
 
 public class MainActivity extends Activity {
-
+ public int a;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
